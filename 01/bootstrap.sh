53e9515cec6d5e1819def16837bccfef81c34fef
mmmarceleza <58913502+mmmarceleza@users.noreply.github.com>
2023-12-19 14:07:13 -0300

    adding more machines - 01

--- 01/bootstrap.sh
+++ 01/bootstrap.sh
@@ -82,4 +82,7 @@ cat >>/etc/hosts<<EOF
 172.16.16.100   kmaster.example.com     kmaster
 172.16.16.101   kworker1.example.com    kworker1
 172.16.16.102   kworker2.example.com    kworker2
+172.16.16.103   kworker3.example.com    kworker3
+172.16.16.104   kworker4.example.com    kworker4
+172.16.16.105   kworker5.example.com    kworker5
 EOF
